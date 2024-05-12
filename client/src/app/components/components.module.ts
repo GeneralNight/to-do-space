2574a5392b06b142c78bd27b82c8322a263f2c54
Guilherme Tinen <gfmi0911@hotmail.com>
2024-05-12 20:33:13 -0300

    text input component

--- client/src/app/components/components.module.ts
+++ client/src/app/components/components.module.ts
@@ -2,17 +2,22 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { TextInputComponent } from './inputs/text-input/text-input.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TextInputComponent
+  ],
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
     RouterModule,
   ],
-  exports: []
+  exports: [
+    TextInputComponent
+  ]
 })
 export class ComponentsModule { }
